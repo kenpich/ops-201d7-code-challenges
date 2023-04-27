61f7427ffb24cac3586a0c5602e14f041d6e6d62
Kenneth <kennepicho@gmail.com>
2023-04-26 19:39:25 -0600

    ops3

---
+++ bash/learnbash.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script Name:                          Script Name
+#Author:                                Your Name
+#Date of lastest revision:              00/00/0000
+#Purpose:                               Purpose
+
+# Declaration of variables
+
+
+# Declaration of functions
+
+
+#Main
+echo "I am working perfectly"
+
+
+
+
+#End
---
+++ bash/ops3201.sh
@@ -0,0 +1,22 @@
+!/bin/bash
+
+# Script Name:                          ops3201
+#Author:                                Kenneth Pichon
+#Date of lastest revision:              04/26/2023
+#Purpose:                               Login History
+
+# Declaration of variables
+
+
+# Declaration of functions
+
+
+#Main
+function history() {
+    last > history.txt
+}
+history
+echo "This is the login history"
+
+
+#End

99aa300526cabc5907a33a428ab0d70bcaf1bce0
Kenneth <kennepicho@gmail.com>
2023-06-07 17:24:48 -0600

    Ops301c3.sh

--- bash/OPS-301/Ops301c3.sh
+++ bash/OPS-301/Ops301c3.sh
@@ -4,18 +4,19 @@
 #06/07/2023
 
 #Create Directory With Permissions From User Input
-file=text.txt
+file=change_log.txt
 read -p "Please Enter New Directory Name: " -r r1
 read -p "Enter Permissions Number (e.g. 777): " -r r2
 mkdir -p $r1 
-chmod $r2 $r1
+chmod u+r $r2 .txt
 parent=$(dirname "$r1")
 cd "$parent"
+ls -a
 
 #Text Output To Log File
 touch $file
     echo "Created directory: $r1
-Set chmod permissions to: $r2
+Set chmod permissions of files in $r1 to: $r2
 Changed directory to: $r1
 Created: $file" > $file
     mv $file $r1 
@@ -24,7 +25,7 @@ Created: $file" > $file
 sleep .5
 echo "Created Directory: $r1"
 sleep .5
-echo "Set chmod permissions to: $r2"
+echo "Set chmod permissions of $r1 files to: $r2"
 sleep .5
 echo "Changed Directory to: $r1"
 sleep .5

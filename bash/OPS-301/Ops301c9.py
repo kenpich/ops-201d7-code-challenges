74fd1ffa1dbec9f3ff1e83f21cfb69213c93d053
Kenneth <kennepicho@gmail.com>
2023-06-28 17:40:49 -0600

    Ops301c9.py

---
+++ bash/OPS-301/Ops301c9.py
@@ -0,0 +1,14 @@
+#Ops301 Challenge 9
+#Kenneth Pichon
+#06/28/2013
+
+
+for num in range(1, 101):
+    print (num)
+    if num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    elif num % 3 and 5:
+        print("FizzBuzz")
+   
